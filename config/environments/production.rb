d104d70ab773f06c6a762c7cbcd4a1d692382f98
Tomoki Yashima <8yashima@gmail.com>
2018-07-09 12:08:35 +0000

    action4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "INSTACLONE_#{Rails.env}"
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/beautiful-yosemite-65452.git' }
+  config.action_mailer.default_url_options = { host: 'https://beautiful-yosemite-65452.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
