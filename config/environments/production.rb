f74afa439ce87b6acc4bc516fb88145176909c1c
Tomoki Yashima <8yashima@gmail.com>
2018-07-09 12:13:40 +0000

    action5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "INSTACLONE_#{Rails.env}"
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { host: 'https://beautiful-yosemite-65452.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'aqueous-temple-45626' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
